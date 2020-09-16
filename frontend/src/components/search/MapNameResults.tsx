c3b5e878ddc126be4ad139529b681dda55aaf271
Joel Bergman <joel.h.bergman@gmail.com>
2020-09-17 02:02:33 +0300

    Quick fix for map search with multiple results

--- frontend/src/components/search/MapNameResults.tsx
+++ frontend/src/components/search/MapNameResults.tsx
@@ -13,10 +13,10 @@ const MapNameResults = (props: Props) => {
         Maps <small>({props.data.length})</small>
       </h2>
       {props.data.length > 0 ? (
-        <div>
+        <div className="flex flex-wrap">
           {props.data.map((m: KZMap, i: number) => (
             <Link to={`/maps/${m.name}`} key={i}>
-              {m.name}
+              <div className="mr-4 mt-4">{m.name}</div>
             </Link>
           ))}
         </div>
