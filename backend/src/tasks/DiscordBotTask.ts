e6dcd716bea8eec5e7748d0769fc2ca310cd42ce
Joel Bergman <joel.h.bergman@gmail.com>
2020-10-01 01:07:16 +0300

    Resolve Discord user @mentions to user names

--- backend/src/tasks/DiscordBotTask.ts
+++ backend/src/tasks/DiscordBotTask.ts
@@ -7,7 +7,13 @@ import { DISCORD_KEY } from '../util/Config'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const md = require('markdown-it')({ breaks: true })
 
-let newsList = []
+interface NewsItem {
+  message: string
+  footer: string
+  date: number
+}
+
+let newsList: NewsItem[] = []
 
 // bots test 752625802377560136
 // official 329472618220486657
@@ -17,14 +23,51 @@ const getNewsList = (client: Discord.Client): void => {
     client.channels.fetch('329472618220486657').then((c: any) => {
       c.messages.fetch({ limit: 10 }).then((messages) => {
         newsList = []
-        messages.forEach((m: Discord.Message) => {
-          if (m.embeds.length === 0 || m.embeds[0].description.includes('[](__kzstats_ignore__)')) return
-          newsList.push({
-            message: md.render(m.embeds[0].description.replace(/```/g, '\r\n```\r\n')),
-            footer: m.embeds[0].footer.text,
+
+        Promise.all(
+          messages.map((m: Discord.Message) => {
+            if (m.embeds.length === 0 || m.embeds[0].description.includes('[](__kzstats_ignore__)')) return
+
+            let message = m.embeds[0].description
+            const match = message.search('<@')
+
+            if (match > 0) {
+              const sliced = message.slice(match)
+              let id = '',
+                mentionTag = ''
+              for (let i = 0; i < 50; i++) {
+                mentionTag += sliced[i]
+                if (!isNaN(parseInt(sliced[i]))) id += sliced[i]
+                if (sliced[i] === '>') break
+              }
+
+              client.users
+                .fetch(id)
+                .then((user) => {
+                  message = message.replace(mentionTag, `\`${user.username}\``)
+                  newsList.push({
+                    message: md.render(message.replace(/```/g, '\r\n```\r\n')),
+                    footer: m.embeds[0].footer.text,
+                    date: Date.parse(m.embeds[0].footer.text.split(' @ ')[1]),
+                  })
+                })
+                .catch((e) => {
+                  logger.error(e.message)
+                })
+            } else {
+              newsList.push({
+                message: md.render(message.replace(/```/g, '\r\n```\r\n')),
+                footer: m.embeds[0].footer.text,
+                date: Date.parse(m.embeds[0].footer.text.split(' @ ')[1]),
+              })
+            }
           })
+        ).then(() => {
+          newsList.sort((a, b) => {
+            return b.date - a.date
+          })
+          console.log('Updated news list')
         })
-        console.log('Updated news list')
       })
     })
   }
